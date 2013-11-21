5971829dcb5ae70957a81c26917857ba767804ec
Logan Gunthorpe <logang@deltatee.com>
2013-11-21 11:38:13 -0700

    Removed permission requirement for FIBMAP.
    Yes, this is a potential security risk. However, the user still needs
    read permission on the device in question and it's preferable to running
    all donard executables as root.

--- fs/ioctl.c
+++ fs/ioctl.c
@@ -55,8 +55,7 @@ static int ioctl_fibmap(struct file *filp, int __user *p)
 	/* do we support this mess? */
 	if (!mapping->a_ops->bmap)
 		return -EINVAL;
-	if (!capable(CAP_SYS_RAWIO))
-		return -EPERM;
+
 	res = get_user(block, p);
 	if (res)
 		return res;
