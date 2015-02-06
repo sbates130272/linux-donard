5ba8447a070ee529e02d45763362bab032db99f5
Andy Whitcroft <apw@canonical.com>
2015-02-06 10:51:27 +0200

    UBUNTU: ubuntu: vbox -- update to 4.3.20-dfsg-1ubuntu1
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

---
+++ ubuntu/vbox/vboxguest/version-generated.h
@@ -0,0 +1,11 @@
+#ifndef ___version_generated_h___
+#define ___version_generated_h___
+
+#define VBOX_VERSION_MAJOR 4
+#define VBOX_VERSION_MINOR 3
+#define VBOX_VERSION_BUILD 20
+#define VBOX_VERSION_STRING_RAW "4.3.20"
+#define VBOX_VERSION_STRING "4.3.20_Ubuntu"
+#define VBOX_API_VERSION_STRING "4_3"
+
+#endif
