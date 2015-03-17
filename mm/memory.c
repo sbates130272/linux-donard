fcb70a99b3d4ed73767cf6f1f8b2defce5b991f0
Logan Gunthorpe <logang@deltatee.com>
2015-03-17 16:15:01 -0600

    Add VM_MIXEDMAP support to follow_pfn
    VM_MIXEDMAPs which are created by mmaping files in DAX capable
    filesystem do have valid PFNs. However, there is no obvious way to get
    the PFN from a VMA because follow_pfn is limitted to VMAs with the
    VM_IO or VM_PFNMAP flags. If we add VM_MIXEDMAP to this check we can
    get the PFN in the same way we do for VM_PFNMAPs.

--- mm/memory.c
+++ mm/memory.c
@@ -3533,7 +3533,7 @@ int follow_pfn(struct vm_area_struct *vma, unsigned long address,
 	spinlock_t *ptl;
 	pte_t *ptep;
 
-	if (!(vma->vm_flags & (VM_IO | VM_PFNMAP)))
+	if (!(vma->vm_flags & (VM_IO | VM_PFNMAP | VM_MIXEDMAP)))
 		return ret;
 
 	ret = follow_pte(vma->vm_mm, address, &ptep, &ptl);
